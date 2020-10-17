cf3f81178903eb52464d4f95d87daabf839cbd44
Jordan Montgomery <montytyper@msn.com>
2020-10-16 21:49:10 -0700

    Initial commit adding stub blake3 provider
    This commit addes the basic provider infrastructure to implement a
    blake3 algorithm provider for OpenSSL. The digest implementation is
    currently a NOP returning zeros.

---
+++ src/blake3.c
@@ -0,0 +1,171 @@
+#include <openssl/core.h>
+#include <openssl/core_dispatch.h>
+#include <openssl/crypto.h>
+#include <openssl/params.h>
+
+#define BLAKE3_DEFAULT_LEN (32)
+#define BLAKE3_MAX_BYTES (64)
+
+OSSL_provider_init_fn ossl_null_provider_init;
+typedef void (*funcptr_t)(void);
+
+struct blake3_ctx {
+	void *prov;
+	size_t out_len;
+};
+
+static void *blake3_newctx(void *vctx)
+{
+	struct blake3_ctx *ctx = OPENSSL_zalloc(sizeof(*ctx));
+
+	if (ctx != NULL) {
+		ctx->prov = vctx;
+		ctx->prov = BLAKE3_DEFAULT_LEN;
+	}
+
+	return ctx;
+}
+
+static void *blake3_dupctx(void *vctx)
+{
+	struct blake3_ctx *src = vctx;
+	struct blake3_ctx *dst = NULL;
+
+	dst = blake3_newctx(NULL);
+
+	if (dst == NULL) {
+		return NULL;
+	}
+
+	dst->prov = src->prov;
+	dst->out_len = src->out_len;
+
+	return dst;
+}
+
+static void blake3_freectx(void *vctx)
+{
+	struct blake3_ctx *ctx = vctx;
+
+	OPENSSL_free(ctx);
+}
+
+int blake3_digest_init(void *vctx)
+{
+	struct blake3_ctx *ctx = vctx;
+
+	return 1;
+}
+
+int blake3_digest_update(void *vctx, const unsigned char *in, size_t inl)
+{
+	struct blake3_ctx *ctx = vctx;
+
+	return 1;
+}
+
+int blake3_digest_final(void *vctx, unsigned char *out, size_t *outl,
+				 size_t outsz)
+{
+	struct blake3_ctx *ctx = vctx;
+	*out = 0;
+
+	return 1;
+}
+
+// clang-format off
+static const OSSL_PARAM blake3_mutable_ctx_params[] = {
+		OSSL_PARAM_size_t(OSSL_DIGEST_PARAM_SIZE, NULL),
+		OSSL_PARAM_END
+};
+// clang-format on
+
+static const OSSL_PARAM *blake3_gettable_ctx_params(void)
+{
+	return blake3_mutable_ctx_params;
+}
+
+static const OSSL_PARAM *blake3_settable_ctx_params(void)
+{
+	return blake3_mutable_ctx_params;
+}
+
+static int blake3_get_ctx_params(void *vctx, , OSSL_PARAM params[])
+{
+	struct blake3_ctx *ctx = vctx;
+	OSSL_PARAM *p;
+
+	if ((p = OSSL_PARAM_locate(params, OSSL_DIGEST_PARAM_SIZE)) != NULL) {
+		return OSSL_PARAM_set_size_t(p, ctx->out_len);
+	}
+
+	return 1;
+}
+
+static int blake3_set_ctx_params(void *vctx, const OSSL_PARAM params[])
+{
+	struct blake3_ctx *ctx = vctx;
+	const OSSL_PARAM *p;
+
+	if ((p = OSSL_PARAM_locate_const(params, OSSL_DIGEST_PARAM_SIZE)) != NULL) {
+		size_t size;
+
+		if (!OSSL_PARAM_get_size_t(p, &size)
+			|| size < 1
+			|| size > BLAKE3_MAX_BYTES) {
+			ERR_raise(ERR_LIB_PROV, PROV_R_NOT_XOF_OR_INVALID_LENGTH);
+			return 0;
+		}
+
+		ctx->out_len = size;
+	}
+
+	return 1;
+}
+
+// clang-format off
+static const OSSL_DISPATCH blake3_functions[] = {
+	{ OSSL_FUNC_DIGEST_NEWCTX, (funcptr_t)blake3_newctx },
+	{ OSSL_FUNC_DIGEST_DUPCTX, (funcptr_t)blake3_dupctx },
+	{ OSSL_FUNC_DIGEST_FREECTX, (funcptr_t)blake3_freectx },
+	{ OSSL_FUNC_DIGEST_INIT, (funcptr_t)blake3_digest_init },
+	{ OSSL_FUNC_DIGEST_UPDATE, (funcptr_t)blake3_digest_update },
+	{ OSSL_FUNC_DIGEST_FINAL, (funcptr_t)blake3_digest_final },
+	{ OSSL_FUNC_DIGEST_GET_CTX_PARAMS, (funcptr_t)blake3_get_ctx_params },
+	{ OSSL_FUNC_DIGEST_SET_CTX_PARAMS, (funcptr_t)blake3_set_ctx_params },
+	{ OSSL_FUNC_DIGEST_GETTABLE_CTX_PARAMS, (funcptr_t)blake3_gettable_ctx_params },
+	{ OSSL_FUNC_DIGEST_SETTABLE_CTX_PARAMS, (funcptr_t)blake3_settable_ctx_params },
+	{ 0, NULL }
+};
+
+static const OSSL_ALGORITHM blake3_digests[] = {
+	{ "blake3", NULL, blake3_functions },
+	{ NULL, NULL, NULL } };
+// clang-format on
+
+/* The function that returns the appropriate algorithm table per operation */
+static const OSSL_ALGORITHM *blake3_operation(void *vprovctx, int operation_id,
+						  int *no_cache)
+{
+	*no_cache = 0;
+	switch (operation_id) {
+	case OSSL_OP_DIGEST:
+		return blake3_digests;
+	}
+	return NULL;
+}
+
+/* The base dispatch table */
+static const OSSL_DISPATCH provider_functions[] = {
+	{ OSSL_FUNC_PROVIDER_QUERY_OPERATION, (funcptr_t)blake3_operation },
+	{ 0, NULL }
+};
+
+int OSSL_provider_init(const OSSL_CORE_HANDLE *handle, const OSSL_DISPATCH *in,
+			   const OSSL_DISPATCH **out, void **provctx)
+{
+	*out = provider_functions;
+	*provctx = (void *)handle;
+
+	return 1;
+}
